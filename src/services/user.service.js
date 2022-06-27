800d2a64068923cceab782964e7fb91a4f8d70f8
Praan26 <arpanap102@gmail.com>
2022-06-27 22:21:04 +0530

    [Arpan]Refactor:User Login refactored with the custom error messages

---
+++ src/services/user.service.js
@@ -0,0 +1,55 @@
+import User from '../models/user.model';
+import bcrypt from 'bcrypt';
+
+//get all users
+export const getAllUsers = async () => {
+  const data = await User.find();
+  return data;
+};
+
+//create new user(USER REGISTRATION)
+export const UserRegistration = async (body) => {
+  const saltRounds = 10
+  const hashpassword = await bcrypt.hash(body.password,saltRounds);
+  body.password = hashpassword;
+  const data = await User.create(body);
+  return data;
+};
+
+//update single user
+export const updateUser = async (_id, body) => {
+  const data = await User.findByIdAndUpdate(
+    {
+      _id
+    },
+    body,
+    {
+      new: true
+    }
+  );
+  return data;
+};
+
+//delete single user
+export const deleteUser = async (id) => {
+  await User.findByIdAndDelete(id);
+  return '';
+};
+
+//get single user(LOGIN)
+export const userLogin = async (body) => {
+  const data = await User.findOne({email:body.email});
+  console.log(data);
+  if(data != null){
+    const comparePassword = await bcrypt.compare(body.password,data.password);
+    if(comparePassword){
+      return data;
+    }
+    else{
+      throw new Error("Password is incorrect");
+    }
+  }
+  else{
+    throw new Error("Email Id does not exist");
+  }
+};
---
+++ src/validators/user.validator.js
@@ -0,0 +1,20 @@
+import Joi from '@hapi/joi';
+
+export const newUserValidator = (req, res, next) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(2).required(),
+    lastName: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      data: data,
+      message: `${error}`
+    });
+  } else {
+    next();
+  }
+};
